0173e3863a450024c5b0f93b35d58b697e3d5b7e
Kyle Cesmat <kyle@kylecesmat.com>
2015-09-12 10:02:01 -0700

    fix conventions.

--- src/Router.js
+++ src/Router.js
@@ -1,12 +1,12 @@
-import React from "react/addons"; // eslint-disable-line
+import React             from "react/addons"; // eslint-disable-line
 import {Route, Redirect} from "react-router";
 
 import App      from "./ui/layouts/app";
 import HomePage from "./ui/pages/home";
 
 export default (
-  <Route name="App" path="/" handler={App}>
-    <Route name="HomePage" path="home" handler={HomePage} />
-    <Redirect from="/" to="HomePage" />
-  </Route>
+    <Route name="app" path="/" handler={App}>
+        <Route name="homePage" path="/home" handler={HomePage} />
+        <Redirect from="/" to="homePage" />
+    </Route>
 );
