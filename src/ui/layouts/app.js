e6bb50d5cb6048264b7397e837a5d54cdfbd7057
Kyle Cesmat <kyle@kylecesmat.com>
2015-09-12 09:55:27 -0700

    add base sass structure and normalize.

--- src/ui/layouts/app.js
+++ src/ui/layouts/app.js
@@ -1,6 +1,7 @@
 import React            from "react/addons";
 import { RouteHandler } from "react-router";
 
+require('normalize.css');
 require('../scss/app.scss');
 
 let Application = React.createClass({
